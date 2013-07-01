1a144a4ce60c9ff3e8b88220e1561d874d1f70b0
David Fisher <tibbon+github@gmail.com>
2013-07-01 14:58:53 -0400

    pushing

--- main.rb
+++ main.rb
@@ -17,21 +17,36 @@ get '/' do
 end
 
 get '/houses' do
-  sql = "SELECT * FROM HOUSES"
+  sql = "SELECT * FROM houses"
   @houses = run_sql(sql)
   erb :houses
 end
 
+post '/houses' do
+  name = params[:name]
+  sigil = params[:sigil]
+  motto = params[:motto]
+  sql = "INSERT INTO houses (name, sigil, motto) VALUES ('Stark', 'http://fc07.deviantart.net/fs70/f/2012/141/d/3/game_of_thrones_house_stark_sigil_render_by_titch_ix-d50m12c.png', 'Winter is Coming');"
+  run_sql(sql)
+  redirect to '/houses'
+end
+
 get '/people' do
-  sql = "SELECT * FROM PEOPLE"
+  sql = "SELECT * FROM people"
   @people = run_sql(sql)
   erb :people
 end
 
 get '/people/:id' do
+  id = params[:id]
+  sql = "SELECT * FROM people WHERE id = #{id}"
+  @person = run_sql(sql).first
   erb :person
 end
 
 get '/houses/:id' do
+  id = params[:id]
+  sql = "SELECT * FROM houses WHERE id = #{id}"
+  @house = run_sql(sql).first
   erb :house
 end
